d7f5600b4f7537f1437f15b4e231dbddd954de12
Joy Wanjiru Gaitho <cirujoygaitho7103@gmail.com>
2026-02-02 15:33:46 +0300

    Building Custom Views and Generic Views in Django REST Framework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics, filters
+from django_filters import rest_framework
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    #  Enable filtering, searching, ordering
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+
+    # Filter by exact matches
+    filterset_fields = ["title", "author", "publication_year"]
+
+    # Search in these fields (case-insensitive)
+    search_fields = ["title", "author"]
+
+    # Allow ordering
+    ordering_fields = ["title", "publication_year"]
+    ordering = ["title"]  # default ordering
