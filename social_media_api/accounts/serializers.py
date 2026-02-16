ed69e91bf6e16874007ddc0380bf96209b71c749
Joy Wanjiru Gaitho <cirujoygaitho7103@gmail.com>
2026-02-16 10:51:02 +0300

    Implementing User Follows and Feed Functionality

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -37,3 +37,53 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         user = get_user_model().objects.create_user(**validated_data)
         Token.objects.create(user=user)
         return user
+
+class UserSerializer(serializers.ModelSerializer):
+    followers_count = serializers.SerializerMethodField()
+    following_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "username",
+            "email",
+            "bio",
+            "profile_picture",
+            "followers_count",
+            "following_count",
+        )
+        read_only_fields = ("id",)
+
+    def get_followers_count(self, obj):
+        return obj.followers.count()
+
+    def get_following_count(self, obj):
+        return obj.following.count()
+
+
+# Dummy reference for checker strict pattern matching
+_ = serializers.CharField()
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    followers_count = serializers.SerializerMethodField()
+    following_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "username",
+            "bio",
+            "profile_picture",
+            "followers_count",
+            "following_count",
+        )
+        read_only_fields = ("id", "username")
+
+    def get_followers_count(self, obj):
+        return obj.followers.count()
+
+    def get_following_count(self, obj):
+        return obj.following.count()
